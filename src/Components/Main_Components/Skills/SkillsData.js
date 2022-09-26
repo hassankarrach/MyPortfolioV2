94343d8cc68a79100bc72865cfe121e14de47845
HassanKarrach <hassan.winners@gmail.com>
2022-09-26 12:50:30 +0100

    updating skills

--- src/Components/Main_Components/Skills/SkillsData.js
+++ src/Components/Main_Components/Skills/SkillsData.js
@@ -1,82 +1,79 @@
 //BlockChainicons
-import openzeppelin from '../../../Media/Skills/icons/BlockChain/openzeppelin.png'
-import MetaMask from '../../../Media/Skills/icons/BlockChain/MetaMask.png'
-import Solidity from '../../../Media/Skills/icons/BlockChain/Solidity.png'
-import Remix from '../../../Media/Skills/icons/BlockChain/Remix.png'
-import Truffle from '../../../Media/Skills/icons/BlockChain/Truffle.png'
-import EtherJs from '../../../Media/Skills/icons/BlockChain/EtherJs.png'
-import Web3 from '../../../Media/Skills/icons/BlockChain/Web3.png'
-import HardHat from '../../../Media/Skills/icons/BlockChain/HardHat.png'
+import openzeppelin from "../../../Media/Skills/icons/BlockChain/openzeppelin.png";
+import MetaMask from "../../../Media/Skills/icons/BlockChain/MetaMask.png";
+import Solidity from "../../../Media/Skills/icons/BlockChain/Solidity.png";
+import Remix from "../../../Media/Skills/icons/BlockChain/Remix.png";
+import Truffle from "../../../Media/Skills/icons/BlockChain/Truffle.png";
+import EtherJs from "../../../Media/Skills/icons/BlockChain/EtherJs.png";
+import Web3 from "../../../Media/Skills/icons/BlockChain/Web3.png";
+import HardHat from "../../../Media/Skills/icons/BlockChain/HardHat.png";
 //WebIcons
-import React from '../../../Media/Skills/icons/WebDev/React.png'
-import Html from '../../../Media/Skills/icons/WebDev/Html.png'
-import Css from '../../../Media/Skills/icons/WebDev/Css.png'
-import JavaScript from '../../../Media/Skills/icons/WebDev/JavaScript.png'
-import NodeJs from '../../../Media/Skills/icons/WebDev/NodeJs.png'
-import CSharp from '../../../Media/Skills/icons/WebDev/CSharp.png'
-import Php from '../../../Media/Skills/icons/WebDev/Php.png'
-import Sql from '../../../Media/Skills/icons/WebDev/Sql.png'
-import LocomotiveScroll from '../../../Media/Skills/icons/WebDev/LocomotiveScroll.png'
-import Gsap from '../../../Media/Skills/icons/WebDev/Gsap.png'
-import StyledComponents from '../../../Media/Skills/icons/WebDev/StyledComponents.png'
+import React from "../../../Media/Skills/icons/WebDev/React.png";
+import NextJs from "../../../Media/Skills/icons/WebDev/NextJs.png";
+import TypeScript from "../../../Media/Skills/icons/WebDev/TypeScript.png";
+import Html from "../../../Media/Skills/icons/WebDev/Html.png";
+import Css from "../../../Media/Skills/icons/WebDev/Css.png";
+import JavaScript from "../../../Media/Skills/icons/WebDev/JavaScript.png";
+import NodeJs from "../../../Media/Skills/icons/WebDev/NodeJs.png";
+import CSharp from "../../../Media/Skills/icons/WebDev/CSharp.png";
+import NestJs from "../../../Media/Skills/icons/WebDev/NestJs.png";
+import Sql from "../../../Media/Skills/icons/WebDev/Sql.png";
+import LocomotiveScroll from "../../../Media/Skills/icons/WebDev/LocomotiveScroll.png";
+import Gsap from "../../../Media/Skills/icons/WebDev/Gsap.png";
+import StyledComponents from "../../../Media/Skills/icons/WebDev/StyledComponents.png";
 //DesignIcons
-import Photoshop from '../../../Media/Skills/icons/Design/Photoshop.png'
-import Illustrator from '../../../Media/Skills/icons/Design/Illustrator.png'
-import PremierPro from '../../../Media/Skills/icons/Design/PremierPro.png'
-
-
-
-
+import Photoshop from "../../../Media/Skills/icons/Design/Photoshop.png";
+import Illustrator from "../../../Media/Skills/icons/Design/Illustrator.png";
+import PremierPro from "../../../Media/Skills/icons/Design/PremierPro.png";
 
 export const Blockchain = [
-    {name :'openzeppelin',alt :'openzeppelin', url :openzeppelin},
-    {name :'MetaMask',alt :'MetaMask',  url :MetaMask},
-    {name :'Solidity', alt:'Solidity',    url :Solidity},
-    {name :'Remix',  alt :'Remix'     ,url :Remix},
-    {name :'Truffle', alt :'Truffle'  ,  url :Truffle},
-    {name :'EtherJs',  alt:'EtherJs' , url :EtherJs},
-    {name :'Web3',   alt  :'Web3'   , url :Web3},
-    {name :'HardHat',  alt :'HardHat',   url :HardHat}
-]
+  { name: "openzeppelin", alt: "openzeppelin", url: openzeppelin },
+  { name: "MetaMask", alt: "MetaMask", url: MetaMask },
+  { name: "Solidity", alt: "Solidity", url: Solidity },
+  { name: "Remix", alt: "Remix", url: Remix },
+  { name: "Truffle", alt: "Truffle", url: Truffle },
+  { name: "EtherJs", alt: "EtherJs", url: EtherJs },
+  { name: "Web3", alt: "Web3", url: Web3 },
+  { name: "HardHat", alt: "HardHat", url: HardHat },
+];
 
 export const Web = [
-    {name :'React', alt:'React', url :React},
-    {name :'Html',  alt:'Html',    url :Html},
-    {name :'Css',   alt:'Css',   url :Css},
-    {name :'JavaScript',    alt:'JavaScript',     url :JavaScript},
-    {name :'NodeJs',  alt:'NodeJs',     url :NodeJs},
-    {name :'CSharp',   alt:'CSharp',    url :CSharp},
-    {name :'Php',     alt:'Php',     url :Php},
-    {name :'Sql',   alt:'Sql',    url :Sql}
-]
+  { name: "ReactJs", alt: "React", url: React },
+  { name: "NextJs", alt: "Next Js", url: NextJs },
+  { name: "TypeScript", alt: "Css", url: TypeScript },
+  { name: "JavaScript", alt: "JavaScript", url: JavaScript },
+  { name: "NodeJs", alt: "NodeJs", url: NodeJs },
+  { name: "CSharp", alt: "CSharp", url: CSharp },
+  { name: "NestJs", alt: "NestJs", url: NestJs },
+  { name: "Sql", alt: "Sql", url: Sql },
+];
 
 export const Design = [
-    {name :'Photoshop', alt:'Photoshop',url :Photoshop},
-    {name :'Illustrator',  alt:'Illustrator',   url :Illustrator},
-    {name :'PremierPro',  alt:'PremierPro',   url :PremierPro}
-]
+  { name: "Photoshop", alt: "Photoshop", url: Photoshop },
+  { name: "Illustrator", alt: "Illustrator", url: Illustrator },
+  { name: "PremierPro", alt: "PremierPro", url: PremierPro },
+];
 
-
-export const Portfolio  = [
-    {name :'React',alt:'React', url :React},
-    {name :'Html',   alt:'Html',  url :Html},
-    {name :'JavaScript', alt:'JavaScript', url :JavaScript},
-    {name :'Css',  alt:'Css',   url :Css},
-    {name :'LocomotiveScroll', alt:'LocomotiveScroll', url :LocomotiveScroll},
-    {name :'Gsap', alt:'Gsap', url :Gsap},
-    {name :'StyledComponents', alt:'StyledComponents', url :StyledComponents},
-]
+export const Portfolio = [
+  { name: "React", alt: "React", url: React },
+  { name: "Html", alt: "Html", url: Html },
+  { name: "JavaScript", alt: "JavaScript", url: JavaScript },
+  { name: "Css", alt: "Css", url: Css },
+  { name: "LocomotiveScroll", alt: "LocomotiveScroll", url: LocomotiveScroll },
+  { name: "Gsap", alt: "Gsap", url: Gsap },
+  { name: "StyledComponents", alt: "StyledComponents", url: StyledComponents },
+];
 
 export const abundeez = [
-    {name :'MetaMask',  alt :'MetaMask',   url :MetaMask},
-    {name :'Html', alt:'Html',    url :Html},
-    {name :'JavaScript', alt:'JavaScript', url :JavaScript},
-    {name :'Css', alt:'Css',    url :Css},
-    {name :'EtherJs',   alt:'EtherJs',   url :EtherJs},
-    {name :'Web3',    Web3:'Web3',     url :Web3},
-    {name :'openzeppelin',alt:'openzeppelin', url :openzeppelin},
-    {name :'React',alt:'React', url :React},
-    {name :'Solidity',   alt:'Solidity',  url :Solidity},
-    {name :'Remix',     alt:'Remix',   url :Remix},
-    {name :'StyledComponents', alt:'StyledComponents', url :StyledComponents}
-]
+  { name: "MetaMask", alt: "MetaMask", url: MetaMask },
+  { name: "Html", alt: "Html", url: Html },
+  { name: "JavaScript", alt: "JavaScript", url: JavaScript },
+  { name: "Css", alt: "Css", url: Css },
+  { name: "EtherJs", alt: "EtherJs", url: EtherJs },
+  { name: "Web3", Web3: "Web3", url: Web3 },
+  { name: "openzeppelin", alt: "openzeppelin", url: openzeppelin },
+  { name: "React", alt: "React", url: React },
+  { name: "Solidity", alt: "Solidity", url: Solidity },
+  { name: "Remix", alt: "Remix", url: Remix },
+  { name: "StyledComponents", alt: "StyledComponents", url: StyledComponents },
+];
